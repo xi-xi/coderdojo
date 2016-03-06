06c94895c5b6772f5a280e6f249562371470b703
xi-xi <qhn.0618@gmail.com>
2016-03-06 23:24:43 +0900

    Add init

--- csgame/csgame/csgame/LogInForm.cs
+++ csgame/csgame/csgame/LogInForm.cs
@@ -15,6 +15,39 @@ namespace csgame
         public LogInForm()
         {
             InitializeComponent();
+            this.loginButton.Click += OnLoginButtonClicked;
+            usernameTextbox.Click += OnTextBoxKeyDown;
+            passwordTextbox.Click += OnTextBoxKeyDown;
+        }
+
+        private void OnTextBoxKeyDown(object sender, EventArgs e)
+        {
+            OnLoggingIn();
+        }
+
+        private void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            OnLoggingIn();
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                OnLoggingIn();
+            }
+            base.OnKeyDown(e);
+        }
+
+        public void OnLoggingIn()
+        {
+            LogIn(usernameTextbox.Text, passwordTextbox.Text);
+        }
+
+        public void LogIn(string username, string password)
+        {
+            var f = new MainForm();
+            f.Show();
         }
     }
 }
