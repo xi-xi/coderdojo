ec3d70b2ffbbc5b9c2d4da8914c8c42705cb30f3
xi-xi <qhn.0618@gmail.com>
2016-03-06 23:12:26 +0900

    Main call loginform

--- csgame/csgame/csgame/Program.cs
+++ csgame/csgame/csgame/Program.cs
@@ -16,7 +16,7 @@ namespace csgame
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LogInForm());
         }
     }
 }
