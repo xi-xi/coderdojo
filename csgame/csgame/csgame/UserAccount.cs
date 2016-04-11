bbe20053e1e65c681207669ca75cca016eb8ad9e
xi-xi <qhn.0618@gmail.com>
2016-04-11 16:19:46 +0900

    add UserAccount class

---
+++ csgame/csgame/csgame/UserAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csgame
+{
+    class UserAccount
+    {
+        public string Name { get; set; }
+        public string PassWord { get; set; }
+        public string IconSource { get; set; }
+    }
+}
