17138439bbc7b2bc7900980547679d9bdc0bbbea
xi-xi <qhn.0618@gmail.com>
2016-04-19 12:40:38 +0900

    [add]: add monogame

---
+++ csgame/csgame/Game/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Shooter
+{
+    class Player
+    {
+        public Texture2D PlayerTexture;
+        public Vector2 Position;
+        public bool Active;
+        public int Health;
+        public int Width
+        {
+            get { return PlayerTexture.Width; }
+        }
+        public int Height
+        {
+            get { return PlayerTexture.Height; }
+        }
+        public void Initialize(Texture2D texture, Vector2 position)
+        {
+            this.PlayerTexture = texture;
+            this.Position = position;
+            this.Active = true;
+            this.Health = 100;
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                this.PlayerTexture,
+                this.Position,
+                null,
+                Color.White,
+                0f,
+                Vector2.Zero,
+                1f,
+                SpriteEffects.None,
+                0f
+             );
+        }
+    }
+}
